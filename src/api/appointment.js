ca49c18235900d0719e2cae9a5893ba8d51cc2f3
Crown_prime <MyCrown1234@hotmail.com>
2019-08-06 00:57:17 +0800

    预约管理

--- src/api/appointment.js
+++ src/api/appointment.js
@@ -5,4 +5,16 @@ export const getAppointList = (data) => request({
     url: baseUrl + "/preod/list",
     method: "post",
     data: data
-})
+});
+
+export const updateAppointStatus = (data) => request({
+    url: baseUrl + '/preod/approve',
+    method: 'post',
+    data: data
+});
+
+export const AddAppointment = (data) => request({
+    url: baseUrl + '/preod/add',
+    method: 'post',
+    data: data
+});
--- src/views/appointment/index.vue
+++ src/views/appointment/index.vue
@@ -8,14 +8,14 @@
             <!-- 查询 -->
             <el-col :span="19" class="search">
               <div class="search-input">
-                <el-input v-model="searchContent" placeholder="店门名称/客户手机号" size="medium"></el-input>
+                <el-input v-model="tableGet.searchContent" placeholder="店门名称/客户手机号" size="medium"></el-input>
               </div>
-              <el-button size="medium" type="primary">查询</el-button>
+              <el-button size="medium" type="primary" @click="tableDateGet">查询</el-button>
             </el-col>
 
             <!-- 新增 -->
             <el-col :span="5" class="add">
-              <el-button icon="el-icon-plus" size="medium" type="primary">新增</el-button>
+              <el-button icon="el-icon-plus" size="medium" type="primary" @click="handleAddAppointDialog">新增</el-button>
             </el-col>
           </el-row>
           <el-row>
@@ -29,36 +29,52 @@
             </el-col>
             <el-col :span="18">
               <!-- 表格 -->
-              <el-table :data="tableData.content">
+              <el-table :data="tableData.content" style="width: 100%">
                 <el-table-column
                   prop="custName"
-                  label="客户名称">
+                  label="客户名称"
+                  width="120">
                 </el-table-column>
                 <el-table-column
                   prop="custMobile"
-                  label="客户手机号">
+                  label="客户手机号"
+                  width="150">
                 </el-table-column>
                 <el-table-column
                   prop="intentionAmount"
-                  label="意向购车金额">
+                  label="意向购车金额"
+                  width="150">
                 </el-table-column>
                 <el-table-column
                   prop="salerName"
-                  label="购车顾问">
+                  label="购车顾问"
+                  width="120">
                 </el-table-column>
                 <el-table-column
                   prop="orgName"
-                  label="所属门店">
+                  label="所属门店"
+                  width="150">
                 </el-table-column>
                 <el-table-column
-                  label="门店地址">
+                  label="状态"
+                  width="120">
+                  <template slot-scope="scope">
+                    <span :class="{ 'appointed': scope.row.status == 0}">{{scope.row.statusDesc}}</span>
+                  </template>
                 </el-table-column>
                 <el-table-column
-                  prop="statusDesc"
-                  label="状态">
+                  label="门店地址"
+                  width="250">
                 </el-table-column>
                 <el-table-column
-                  label="操作">
+                  label="操作"
+                  fixed="right"
+                  width="200">
+                  <template slot-scope="scope">
+                    <el-button size="mini" v-if="scope.row.status != 0" @click="handleUpdateAppoint(scope.row, 0)">恢复为预约</el-button>
+                    <el-button size="mini" type="primary" v-if="scope.row.status == 0" @click="handleUpdateAppoint(scope.row, 1)">通  过</el-button>
+                    <el-button size="mini" type="primary" v-if="scope.row.status == 0" @click="handleUpdateAppoint(scope.row, 2)">不通过</el-button>
+                  </template>
                 </el-table-column>
               </el-table>
               <div class="table-foot">
@@ -72,13 +88,71 @@
               </div>
             </el-col>
           </el-row>
+
+          <!-- 菜单弹窗 -->
+          <el-dialog title="新增预约" :visible.sync="addAppointDialog">
+            <el-form label-width="120px" :model="addAppoint" :rules="rules" ref="addAppointForm">
+              <el-form-item label="客户名称" prop="custId">
+                <el-select v-model="addAppoint.custId" size="medium">
+                  <el-option
+                    v-for="(item, index) in customerInfo"
+                    :key="index"
+                    :value="item.custId"
+                    :label="item.custName"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="客户手机号">
+                <el-input size="medium" :disabled="true" v-model="comAddAppointCustMobild"></el-input>
+              </el-form-item>
+              <el-form-item label="所属门店" prop="orgId">
+                <el-cascader
+                  v-model="addAppoint.orgId"
+                  size="medium"
+                  :options="cascaderOrgTree"
+                  :props="defaultProps"
+                  :show-all-levels="false">
+                </el-cascader>
+              </el-form-item>
+              <el-form-item label="意向金额" prop="intentionAmount">
+                <el-input size="medium" v-model="addAppoint.intentionAmount"></el-input>
+              </el-form-item>
+              <el-form-item label="购车顾问" prop="salerId">
+                <el-select v-model="addAppoint.salerId" size="medium">
+                  <el-option
+                    v-for="(item, index) in staffInfo"
+                    :key="index"
+                    :value="item.id"
+                    :label="item.nickname"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="状态" prop="status">
+                <el-select v-model="addAppoint.status" size="medium">
+                  <el-option :value="0" label="预约"></el-option>
+                  <el-option :value="1" label="审批通过"></el-option>
+                  <el-option :value="2" label="审批不通过"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="地址" prop="orgAddress">
+                <el-input
+                  type="textarea"
+                  v-model="addAppoint.orgAddress"
+                  :autosize="{ minRows: 2, maxRows: 4 }"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button size="medium" type="primary" @click="handleAddAppoint">提 交</el-button>
+              </el-form-item>
+            </el-form>
+          </el-dialog>
+
         </div>
     </template>
   </basic-container>
 </template>
 
 <script>
-import { getAppointList } from '@/api/appointment';
+import { getAppointList, updateAppointStatus, AddAppointment } from '@/api/appointment';
+import { getCustomerList } from '@/api/customer';
+import { getStaffList } from '@/api/staff';
 import { mapGetters } from "vuex";
 import { deepClone } from '@/util/util';
 export default {
@@ -86,13 +160,52 @@ export default {
     ...mapGetters(['orgTree']),
     comOrgTree() {
       return [{ orgFullName: "全部", id: "", children: this.orgTree }];
-    }
+    },
+    comAddAppointCustMobild() {
+      let temp = "";
+      if(this.addAppoint.custId != "") {
+        temp = this.customerInfo.find((item) => {
+          return this.addAppoint.custId == item.custId;
+        }).custMobile;
+      }
+      return temp;
+    },
+    cascaderOrgTree() {
+      let corgTree = deepClone(this.orgTree);
+      corgTree.forEach((item) => {
+        this.cascaderMenuRe(item);
+      });
+      return corgTree;
+    },
   },
   data() {
     return {
       defaultProps: {
         children: 'children',
-        label: 'orgFullName'
+        label: 'orgFullName',
+        value: 'id',
+        checkStrictly: true,
+        emitPath: false
+      },
+      rules: {
+        custId: [
+          { required: true, message: '客户名称不能为空', trigger: 'blur' }
+        ],
+        orgId: [
+          { required: true, message: '请选择所属机构', trigger: 'blur' }
+        ],
+        intentionAmount: [
+          { required: true, message: '请填写意向金额', trigger: 'blur' }
+        ],
+        salerId: [
+          { required: true, message: '请选择购车顾问', trigger: 'blue' }
+        ],
+        status: [
+          { required: true, message: '请选择预约状态', trigger: 'blue' }
+        ],
+        orgAddress: [
+          { required: true, message: '请填写地址', trigger: 'blue' }
+        ]
       },
       tableData: {},
       tableGet: {
@@ -102,6 +215,18 @@ export default {
         pageSize: 10,
         searchContent: ""
       },
+      addAppointDialog: false,
+      customerInfo: [],
+      staffInfo: [],
+      addAppoint: {
+        custId: "",
+        custMobile: "",
+        intentionAmount: "",
+        salerId: "",
+        orgId: "",
+        orgAddress: "",
+        status: ""
+      }
     }
   },
   created() {
@@ -114,11 +239,10 @@ export default {
   },
   methods: {
     tableDateGet() {
-      const that = this;
-      getAppointList(that.tableGet).then((res) => {
+      getAppointList(this.tableGet).then((res) => {
         if(res.data.code === 0) {
           const data = res.data.data;
-          that.$set(that, 'tableData', deepClone(data));
+          this.$set(this, 'tableData', deepClone(data));
         }
       }).catch(()=> {
         this.$notify.error({ title: '获取数据失败', message: '网络连接错误。' });
@@ -130,6 +254,64 @@ export default {
     },
     handleCurrentChange() {
       this.tableDateGet();
+    },
+    handleUpdateAppoint(obj, status) {
+      updateAppointStatus({
+        id: obj.id,
+        status: status,
+        salerId: ""
+      }).then((res) => {
+        if(res.data.code === 0) {
+          this.$notify.success({ title: '审批成功', message: '预约状态已修改。' });
+          this.tableDateGet();
+        }
+      })
+    },
+    handleAddAppointDialog() {
+      Promise.all([
+        getCustomerList({
+          paging: false,
+          pageNum: "",
+          pageSize: ""
+        }),
+        getStaffList({
+          type: 1,
+          paging: false,
+          pageNum: "",
+          pageSize: ""
+        })
+      ]).then((resArr) => {
+        const data0 = resArr[0].data;
+        const data1 = resArr[1].data;
+        if(data0.code === 0 && data1.code === 0) {
+          this.$set(this, 'customerInfo', deepClone(data0.data.content));
+          this.$set(this, 'staffInfo', deepClone(data1.data.content));
+          this.addAppointDialog = true;
+        }
+      });
+    },
+    cascaderMenuRe(item) {
+      item.disabled = false;
+      if(item.children.length === 0) {
+        delete item.children;
+      } else {
+        item.children.forEach((item) => {
+          this.cascaderMenuRe(item);
+        });
+      }
+    },
+    handleAddAppoint() {
+      this.$refs.addAppointForm.validate((val) => {
+        if(val) {
+          AddAppointment(this.addAppoint).then((res) => {
+            if(res.data.code === 0) {
+              this.$notify.success({ title: '增加成功', message: '预约列表已增加。' });
+              this.addAppointDialog = false;
+              this.tableDateGet();
+            }
+          })
+        }
+      })
     }
   }
 }
@@ -160,5 +342,9 @@ export default {
   display: flex;
   flex-direction: row-reverse;
   margin-top: 20px;
+}
+
+.appointed {
+  color: #67C23A;
 }
 </style>
