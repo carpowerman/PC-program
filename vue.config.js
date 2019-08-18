ef5ede33074ca3d4843b367a67483fc746445be9
Crown_prime <MyCrown1234@hotmail.com>
2019-08-18 12:36:30 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // 基础路径 注意发布之前要先修改这里
-let publicPath = '/cls'
+let publicPath = '/'
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
     publicPath: publicPath, // 根据你的实际情况更改这里
